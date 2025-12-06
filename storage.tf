20764252a7fad7322f3fc3517c30c5c467328841
Gbèdossou Harold Scifo LEGBA <gbedossou.legba@efrei.net>
2025-12-06 15:53:44 +0000

    Edit storage.tf

--- storage.tf
+++ storage.tf
@@ -10,9 +10,7 @@ resource "azurerm_storage_account" "sa" {
 }
 
 resource "azurerm_storage_container" "container" {
-  name                  = var.storage_container_name
+  name                 = var.storage_container_name
+  storage_account_name = azurerm_storage_account.sa.name
   container_access_type = "private"
 }
-
-
-
